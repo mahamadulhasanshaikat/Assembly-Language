c9b19d80e577c9126de0c3efaea003f93c017c98
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-12 19:37:39 +0600

    String

---
+++ Sum/Sum.asm
@@ -0,0 +1,33 @@
+;ADDITION OF TWO NUMBERS
+.MODEL SMALL
+.STACK 100H  
+                                  ;2+3 = 5? = 53 ASCII
+                                  ;2 = 50 ASCII
+                                  ;3 = 51 ASCII 
+                                  ;2 + 3 = (50 + 51) ASCII = 101 = e
+                                  ;101 - 53 = 48
+                                  
+.CODE
+
+MAIN PROC
+    
+    MOV AH,1
+    INT 21H
+       
+    MOV CL, AL
+     
+    INT 21H       
+    MOV BL, AL 
+    
+          
+    ADD CL,BL  ;CL=CL+BL
+    
+             
+    SUB CL,48
+     
+          
+    MOV AH, 2           
+    MOV DL, CL ;OUTPUT
+    INT 21H
+    
+END MAIN
